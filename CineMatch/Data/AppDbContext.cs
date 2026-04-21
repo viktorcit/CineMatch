a3d90673caf602fb32dccda67b8931965f1c79c0
viktorcit <vcudik44@gmail.com>
2026-04-21 21:03:03 +0300

    feat: Add database and movie model

---
+++ CineMatch/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CineMatch.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CineMatch.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies => Set<Movie>();
+    }
+}
---
+++ CineMatch/Model/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace CineMatch.Model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; } = null!; 
+        public required string Genre { get; set; } = null!;
+        public int ReleaseYear { get; set; }
+        public required string Discription { get; set; } = null!;
+    }
+}
